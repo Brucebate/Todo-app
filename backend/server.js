f31737e7a863f9af166fd6f83ec4588a900cba96
Priyanshu Rangari <priyanshurangari60@gmail.com>
2024-12-11 18:15:12 +0530

    add code

--- backend/server.js
+++ backend/server.js
@@ -84,8 +84,8 @@ app.delete('/todos/:id', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
