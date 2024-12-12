21401a626c39b1a6c94d89f370e9c99f54f74d6f
Priyanshu Rangari <priyanshurangari60@gmail.com>
2024-12-12 12:39:04 +0530

    momgo true things changes

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
