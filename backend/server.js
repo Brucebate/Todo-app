9584cb260a706d31b73a420c247afc611e22828b
Priyanshu Rangari <priyanshurangari60@gmail.com>
2024-12-12 12:20:20 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-// Middleware
+// MiddlewareS
 app.use(cors());
 app.use(bodyParser.json());
 
